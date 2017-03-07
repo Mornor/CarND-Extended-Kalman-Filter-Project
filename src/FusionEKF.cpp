bb20ddfae5c2185d543af0d56ea31cdc9cea6af7
Celien Nanson <cesliens@gmail.com>
2017-03-07 19:40:17 +0100

    Initialize EKF matrices and implenent Predict() and Update() functions

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -12,20 +12,55 @@ using std::vector;
  * Constructor.
  */
 FusionEKF::FusionEKF() {
-  is_initialized_ = false;
+    is_initialized_ = false;
 
-  previous_timestamp_ = 0;
+    previous_timestamp_ = 0;
 
-  // initializing matrices
-  R_laser_ = MatrixXd(2, 2);
-  R_radar_ = MatrixXd(3, 3);
-  H_laser_ = MatrixXd(2, 4);
-  Hj_ = MatrixXd(3, 4);
+    // initializing matrices
 
-  /**
-  TODO:
-    * Finish initializing the FusionEKF.
-  */
+    // state covariance matrix P
+    P_ = MatrixXd(4, 4);
+    P_ <<   1, 0, 0, 0,
+            0, 1, 0, 0,
+            0, 0, 1000, 0,
+            0, 0, 0, 1000;
+
+    // measurement covariance for the laser
+    R_laser_ = MatrixXd(2, 2);
+    R_laser_ << 0.0225, 0,
+                0, 0.0225;
+
+    // measurement matrix for the laser
+    H_laser_ = MatrixXd(2, 4);
+    H_laser_ << 1, 0, 0, 0,
+                0, 1, 0, 0;
+
+    // measurement covariance for the radar
+    R_radar_ = MatrixXd(3, 3);
+    R_radar_ << 0.0225, 0,
+                0, 0.0225, 0, 
+                0, 0, 0.0225;
+    
+    // Jacobian matrix
+    Hj_ = MatrixXd(3, 4);
+
+    // initial transition matrix F_
+    F_ = MatrixXd(4, 4);
+    F_ <<   1, 0, 1, 0,
+            0, 1, 0, 1,
+            0, 0, 1, 0,
+            0, 0, 0, 1;
+
+    VectorXd x_ = VectorXd(4);
+    x_ << 1, 1, 1, 1;
+
+    MatrixXd Q_ = MatrixXd(4, 4);
+
+    ekf_.Init(x_, P_, F_, H_laser_, R_laser_, Q_);
+
+    // set the acceleration noise components
+    noise_ax = 5;
+    noise_ay = 5;
 }
 
 /**
